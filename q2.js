d16ca01053d4b81383af30f4b0d9d5869a51179a
GabiDuartte <93677092+GabiDuartte@users.noreply.github.com>
2022-08-10 00:01:32 -0300

    Delete q2.js

--- q2.js
+++
@@ -1,7 +0,0 @@
-const prompt = require('prompt-sync')();
-
-const real = Number(prompt('Informe um valor em real: '))
-
-const bitcoin = real*118.98664
-
-console.log('O valor em bitcoin será: ', Math.round(bitcoin))
